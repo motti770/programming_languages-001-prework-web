c83a72422fc2d0f88a45820ff3bfc004697d40ce
motti770 <mottigottlieb@gmail.com>
2016-02-22 23:02:32 +0000

    nitrous-backup at Mon Feb 22 23:02:32 UTC 2016

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,19 @@
+require 'pry'
+
+
+#   it 'reformats ruby properly' do
+#     ruby = {:type => "interpreted", :style => [:oo]}
+#     ruby.each { |k,v| expect(result[:ruby][k]).to eq(v) }
+#   end
+
+
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  languages.each do |type, original_key_hash |
+    original_key_hash.each do |language, soon|
+      new_hash[language] = { soon.keys => soon.values}
+
+end
+end
+
 end
